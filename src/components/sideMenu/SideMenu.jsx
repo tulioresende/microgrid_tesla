d2ac6e2239d131474cf7c1e8a451f43a9f217311
tulioresende <tulio.resende.ufmg@gmail.com>
2021-04-16 15:43:51 -0300

    adicao de rota para dashboards a partir do menu lateral

--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 import TeslaIcon from '../../assets/jpeg/logoteslaV2.jpeg';
 
@@ -13,20 +14,23 @@ import {
 
 const SideMenu = ({visible=true}) =>{
 
-        return (
-            <Container width={visible?"256":"0"}>
-                <Image src={TeslaIcon}/>
-                <MenuTitle>
-                    Menu Principal
-                </MenuTitle>
-                <MenuItem>
-                    Dashboards
-                </MenuItem>
-                <MenuItem>
-                    Controles
-                </MenuItem>
-            </Container>
-        );
+    const history = useHistory();
+    const handleOnClick = () => history.push(`/dashboards`);
+
+    return (
+        <Container width={visible?"256":"0"}>
+            <Image src={TeslaIcon}/>
+            <MenuTitle>
+                Menu Principal
+            </MenuTitle>
+            <MenuItem onClick={handleOnClick}>
+                Dashboards
+            </MenuItem>
+            <MenuItem>
+                Controles
+            </MenuItem>
+        </Container>
+    );
 }
 
 export default SideMenu;
