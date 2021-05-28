12ad53f53f8be8cb51610d8747f02ec1ea8b614c
tulioresende <tulio.resende.ufmg@gmail.com>
2021-05-28 14:08:13 -0300

    adicao de pagina de visualizacao de energia gerada

---
+++ src/pages/EnergyGeneration.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+import Cards from "../../components/cards";
+import PageStructure from "../../components/pageStructure/PageStructure";
+import getBatteriesCurrentData from "../../firebase/services/batteries/Batteries";
+import { BatteryStatusTranslate } from "../../utils/functions/Batteries";
+import { sleep } from "../../utils/functions/General";
+
+const EnergyGeneration = () => {
+  const [batteriesData, setBatteriesData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [trigger, setTrigger] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      const list = await getBatteriesCurrentData();
+      setBatteriesData(list);
+      setIsLoading(false);
+    };
+    getData();
+  }, [trigger]);
+
+  const configTrigger = async () => {
+    while (true) {
+      await sleep(5);
+      setTrigger(!trigger);
+    }
+  };
+
+  const renderFunction = () => {
+    configTrigger();
+    return (
+      <>
+        {isLoading ? (
+          <h1>Carregando...</h1>
+        ) : (
+          batteriesData.map((battery) => (
+            <Cards.CardStatusBattery
+              name={battery.nome}
+              current={battery.corrente}
+              voltage={battery.tensao}
+              status={BatteryStatusTranslate(battery.status)}
+              chargeState={battery.estadoCarga}
+            />
+          ))
+        )}
+      </>
+    );
+  };
+  return (
+    <PageStructure renderFunction={renderFunction} pageTitle={"Baterias"} />
+  );
+};
+
+export default EnergyGeneration;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
-import Dashboards from './dashboards/Dashboards';
-import NotFound from './notFound/NotFound';
-import Batteries from './batteries/Batteries';
-import Login from './login/Login';
-import MainGrid from './mainGrid/MainGrid';
-
+import Dashboards from "./dashboards/Dashboards";
+import NotFound from "./notFound/NotFound";
+import Batteries from "./batteries/Batteries";
+import Login from "./login/Login";
+import MainGrid from "./mainGrid/MainGrid";
+import EnergyGeneration from "./EnergyGeneration";
 
 const Pages = {
-    Dashboards,
-    NotFound,
-    Batteries,
-    Login,
-    MainGrid,
-}
+  Dashboards,
+  NotFound,
+  Batteries,
+  Login,
+  MainGrid,
+  EnergyGeneration,
+};
 
 export default Pages;
