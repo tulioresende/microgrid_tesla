f629fc2ba2e68db1befbac1321179610ec920b02
tulioresende <tulio.resende.ufmg@gmail.com>
2021-06-02 14:23:58 -0300

    ajuste para retirar casas decimais das medidas de tensao e corrente das baterias

--- src/pages/batteries/Batteries.jsx
+++ src/pages/batteries/Batteries.jsx
@@ -1,56 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Cards from '../../components/cards';
-import PageStructure from '../../components/pageStructure/PageStructure';
-import getBatteriesCurrentData from '../../firebase/services/batteries/Batteries';
-import { BatteryStatusTranslate } from '../../utils/functions/Batteries';
-import { sleep } from '../../utils/functions/General';
+import Cards from "../../components/cards";
+import PageStructure from "../../components/pageStructure/PageStructure";
+import getBatteriesCurrentData from "../../firebase/services/batteries/Batteries";
+import { BatteryStatusTranslate } from "../../utils/functions/Batteries";
+import { sleep } from "../../utils/functions/General";
 
-const Batteries = () =>{
-    const [batteriesData, setBatteriesData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [trigger, setTrigger] = useState(false);
+const Batteries = () => {
+  const [batteriesData, setBatteriesData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [trigger, setTrigger] = useState(false);
 
-    useEffect(() =>{
-        const getData = async () =>{
-            const list = await getBatteriesCurrentData();
-            setBatteriesData(list);
-            setIsLoading(false);
-        }
-        getData();
-    },[trigger]);
+  useEffect(() => {
+    const getData = async () => {
+      const list = await getBatteriesCurrentData();
+      setBatteriesData(list);
+      setIsLoading(false);
+    };
+    getData();
+  }, [trigger]);
 
-    const configTrigger = async () =>{
-        while (true){
-            await sleep(5);
-            setTrigger(!trigger);
-        }
-    }   
-
-    const renderFunction = () =>{
-        configTrigger();
-        return(
-            <>  
-                {isLoading ? ( 
-                    <h1>
-                        Carregando...
-                    </h1>
-                ):(
-                batteriesData.map((battery) => 
-                    <Cards.CardStatusBattery 
-                        name={battery.nome} 
-                        current={battery.corrente} 
-                        voltage={battery.tensao} 
-                        status={BatteryStatusTranslate(battery.status)} 
-                        chargeState={battery.estadoCarga}
-                    />)
-                )}
-            </>
-        )
+  const configTrigger = async () => {
+    while (true) {
+      await sleep(5);
+      setTrigger(!trigger);
     }
+  };
+
+  const renderFunction = () => {
+    configTrigger();
     return (
-        <PageStructure renderFunction={renderFunction} pageTitle={"Baterias"}/>
+      <>
+        {isLoading ? (
+          <h1>Carregando...</h1>
+        ) : (
+          batteriesData.map((battery) => (
+            <Cards.CardStatusBattery
+              name={battery.nome}
+              current={Math.round(battery.corrente, -1)}
+              voltage={Math.round(battery.tensao, -1)}
+              status={BatteryStatusTranslate(battery.status)}
+              chargeState={battery.estadoCarga}
+            />
+          ))
+        )}
+      </>
     );
-}
+  };
+  return (
+    <PageStructure renderFunction={renderFunction} pageTitle={"Baterias"} />
+  );
+};
 
 export default Batteries;
